4c1b26a802da527508e7617f2e98ee1c9c2aa295
Utilisateur <user@debian.arcahe.ovh>
2017-10-02 15:09:33 +0200

    exercice2-1

---
+++ SimpleFraction/Fraction.java
@@ -0,0 +1,23 @@
+package simpleFraction;
+
+public class Fraction
+{
+	private double numerateur,denominateur;
+	
+	public Fraction(double numerateur, double denominateur)
+	{
+		this.numerateur= numerateur;
+		this.denominateur = denominateur;
+	}
+	
+	public Fraction()
+	{
+		this(0, 0);
+	}
+	
+	@Override
+	public String toString()
+	{
+		return Double.valueOf(numerateur).toString() + " " + Double.valueOf(denominateur).toString();
+	}
+}
---
+++ SimpleFraction/main.java
@@ -0,0 +1,13 @@
+package simpleFraction;
+
+import java.lang.Object;
+
+public class Main
+{
+	public static void main(String args[])
+	{
+		System.out.println("affichage");
+		Fraction frac = new Fraction();
+		System.out.println(frac.toString());
+	}
+}
